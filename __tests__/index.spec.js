da235ddf0e5f4ee6e11fa194e612777c62a893d7
afeiship <1290657123@qq.com>
2021-01-29 22:15:38 +0800

    chore: add slate pkg

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -3,8 +3,6 @@
 
   describe('NxSlateDefaults.methods', function() {
     test('init', function() {
-      const data = { key: 1, value: 2 };
-      expect(!!data).toBe(true);
     });
   });
 })();
